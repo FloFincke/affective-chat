ccd4ee7b90e3fa5e81ebbe71a31a5be75c32e1b2
FloFincke <flofincke.dev@posteo.de>
2017-11-18 18:32:56 +0100

    massive update beims server
    Signed-off-by: FloFincke <flofincke.dev@posteo.de>

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Unser Repo
 
 `affective-chat`: iOS App
 `push-server`: Unser Server halt
+
+## Server HOWTO
+
+Vorher Zugangsdaten etc. vom Flo holen
+
+1. MongoDB, node etc. installieren
+2. im Terminal in `push-server` navigieren
+3. `npm install` laufen lassen
+4. in einem neuen Terminal Fenster die MongoDB starten (`mongod` ausführen)
+5. im ersten Fenster `npm run start` ausführen
---
+++ push-server/routes/device.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const database = require('../components/database');
+const newPushSchedule = require('../components/push');
+
+router.post('/new', function(req, res, next) {
+    if (req.body.username && req.body.token) {
+        const date = new Date();
+        const phone = new database.Phone({
+            username: req.body.username,
+            token: req.body.token,
+            createdAt: date
+        });
+
+        phone.save(function(err, data) {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            } else {
+                console.log(date + ' -- Registered phone of ' + phone.username + ' with token: ' + phone.token);
+
+                //schedule pushes
+                newPushSchedule(data.id);
+
+                res.status(200).send(data.id);
+            }
+        });
+
+    } else {
+        console.log('New device: parameters not provided');
+        return res.sendStatus(422);
+    }
+
+});
+
+module.exports = router;
