3317b811b5bcb8053245f5e6c828654e8039b0b2
Chuck-Aguilar <ca@spendit.de>
2018-01-17 12:04:36 +0100

    data_set day per day

--- classifiers/controller.py
+++ classifiers/controller.py
@@ -1,21 +1,11 @@
-import keras
-import matplotlib.pyplot as plt
 import numpy as np
-import os
-
-from keras.callbacks import ModelCheckpoint
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
 from classifiers.baselines import Classifier
 from classifiers.data_set import produce_data_set
 
-BATCH_SIZE = 128
-HIDDEN_DIM = 175
-
-# NB_EPOCH = 10000
-NB_EPOCH = 2
-PATH = "../vince.csv"
+PATH = "../vince_with_dates.csv"
 
 seed = 155
 np.random.seed(seed)
@@ -34,97 +24,16 @@ scaler = StandardScaler()
 X_train_fitted = scaler.fit_transform(X_train)
 X_test_fitted = scaler.fit_transform(X_test)
 
-classifier = Classifier(HIDDEN_DIM, X_train.shape[1], HIDDEN_DIM, X_train.shape[1])
-# classifier = Classifier(X_train.shape[0], X_train.shape[1], X_train.shape[0], X_train.shape[1])
-
-early_stop_criteria = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0,
-                                                    patience=80, verbose=0, mode='auto')
-model_callbacks_little = [early_stop_criteria]
-model_callbacks_large = [early_stop_criteria]
-file_little_path = "models/nn_little_weights_%dneurons-{epoch:02d}.hdf5" % HIDDEN_DIM
-file_large_path = "models/nn_large_weights_%dneurons-{epoch:02d}.hdf5" % HIDDEN_DIM
-
-checkpoint_little = ModelCheckpoint(file_little_path, monitor='val_acc', verbose=0, save_weights_only=True,
-                                    save_best_only=False, mode='max')
-checkpoint_large = ModelCheckpoint(file_large_path, monitor='val_acc', verbose=0, save_weights_only=True,
-                                   save_best_only=False, mode='max')
-
-model_callbacks_little.append(checkpoint_little)
-model_callbacks_large.append(checkpoint_large)
+classifier = Classifier()
 
 Y_train_dt = Y_train.astype('int')
 Y_test_dt = Y_test.astype('int')
 
 decision_tree = classifier.decision_tree.fit(X_train, Y_train_dt)
-little_nn = classifier.little_nn.fit(X_train_fitted, Y_train, epochs=NB_EPOCH, verbose=2, batch_size=X_train.shape[0],
-                                     callbacks=model_callbacks_little, validation_split=0.2, initial_epoch=0).history
-large_nn = classifier.large_nn.fit(X_train_fitted, Y_train, epochs=NB_EPOCH, verbose=2, batch_size=X_train.shape[0],
-                                   callbacks=model_callbacks_large, validation_split=0.2, initial_epoch=0).history
-
+random_forest = classifier.random_forest.fit(X_train, Y_train_dt)
 
 predictions_tree = decision_tree.predict(X_test)
-
 print('Accuracy Tree: ' + str(accuracy_score(Y_test_dt, predictions_tree)))
 
-test_classifier = Classifier(HIDDEN_DIM, X_train.shape[1], HIDDEN_DIM, X_train.shape[1])
-
-test_over_time_little = []
-test_over_time_large = []
-
-for i in range(len(little_nn['loss'])):
-    test_classifier.little_nn.load_weights("models/nn_little_weights_%dneurons-%02d.hdf5" % (HIDDEN_DIM, i))
-    scores = test_classifier.little_nn.evaluate(X_test_fitted, Y_test, verbose=0)
-    test_over_time_little.append(scores)
-    # remove
-    os.remove("models/nn_little_weights_%dneurons-%02d.hdf5" % (HIDDEN_DIM, i))
-
-for i in range(len(large_nn['loss'])):
-    test_classifier.large_nn.load_weights("models/nn_large_weights_%dneurons-%02d.hdf5" % (HIDDEN_DIM, i))
-    scores = test_classifier.large_nn.evaluate(X_test_fitted, Y_test, verbose=0)
-    test_over_time_large.append(scores)
-    # remove
-    os.remove("models/nn_large_weights_%dneurons-%02d.hdf5" % (HIDDEN_DIM, i))
-
-test_over_time_little = np.array(test_over_time_little)
-test_over_time_large = np.array(test_over_time_large)
-little_nn['test_loss'] = [row[0] for row in test_over_time_little]
-little_nn['test_acc'] = [row[1] for row in test_over_time_little]
-large_nn['test_loss'] = [row[0] for row in test_over_time_large]
-large_nn['test_acc'] = [row[1] for row in test_over_time_large]
-
-print(little_nn)
-print(large_nn)
-
-fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.plot(range(len(little_nn['val_loss'])), little_nn['val_loss'], linestyle='-', color='mediumpurple',
-         label='Validation (Standardised)', lw=2)
-ax1.plot(range(len(little_nn['test_loss'])), little_nn['test_loss'], linestyle='-', color='lightgreen',
-         label='Test (Standardised)', lw=2)
-ax2.plot(range(len(little_nn['val_acc'])), little_nn['val_acc'], linestyle='-', color='mediumpurple',
-         label='Validation (Standardised)', lw=2)
-ax2.plot(range(len(little_nn['test_acc'])), little_nn['test_acc'], linestyle='-', color='lightgreen',
-         label='Test (Standardised)', lw=2)
-leg = ax1.legend(bbox_to_anchor=(0.5, 0.95), loc=2, borderaxespad=0., fontsize=13)
-ax1.set_xticklabels('')
-ax2.set_xlabel('# Epochs', fontsize=14)
-ax1.set_ylabel('Loss Little', fontsize=14)
-ax2.set_ylabel('Accuracy Little', fontsize=14)
-plt.tight_layout()
-plt.show()
-
-fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.plot(range(len(large_nn['val_loss'])), large_nn['val_loss'], linestyle='-', color='mediumpurple',
-         label='Validation (Standardised)', lw=2)
-ax1.plot(range(len(large_nn['test_loss'])), large_nn['test_loss'], linestyle='-', color='lightgreen',
-         label='Test (Standardised)', lw=2)
-ax2.plot(range(len(large_nn['val_acc'])), large_nn['val_acc'], linestyle='-', color='mediumpurple',
-         label='Validation (Standardised)', lw=2)
-ax2.plot(range(len(large_nn['test_acc'])), large_nn['test_acc'], linestyle='-', color='lightgreen',
-         label='Test (Standardised)', lw=2)
-leg = ax1.legend(bbox_to_anchor=(0.5, 0.95), loc=2, borderaxespad=0., fontsize=13)
-ax1.set_xticklabels('')
-ax2.set_xlabel('# Epochs', fontsize=14)
-ax1.set_ylabel('Loss Large', fontsize=14)
-ax2.set_ylabel('Accuracy Large', fontsize=14)
-plt.tight_layout()
-plt.show()
+predictions_random = random_forest.predict(X_test)
+print('Accuracy Forest: ' + str(accuracy_score(Y_test_dt, predictions_random)))
--- classifiers/testClassifiers.py
+++ classifiers/testClassifiers.py
@@ -15,59 +15,24 @@ PATH = "../vince_with_dates.csv"
 data_sets_files = glob.glob(PATH)
 data_set = (pd.concat((pd.read_csv(f, sep=';', header=0) for f in data_sets_files))).values
 
-#X = data_set[:, 3:-1]
-#Y = data_set[:,-1]
-
-#X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.275, random_state = 42)
-
-#print(type(X_train))
-
-#scaler = StandardScaler()
-
-#X_train_fitted	= scaler.fit_transform(X_train)
-#X_test_fitted	= scaler.fit_transform(X_test)
-#y_train_dt		= y_train.astype('int')
-#y_test_dt		= y_test.astype('int')
-
-# DECISION TREE
-#decisionTree = tree.DecisionTreeClassifier()
-#decisionTree.fit(X_train, y_train_dt)
-#predictions_tree = decisionTree.predict(X_test)
-#print('Accuracy DecisionTree: ' + str(accuracy_score(y_test_dt, predictions_tree)))
-
-
-# RANDOM FOREST
-#clf = RandomForestClassifier(n_jobs=2, random_state=0)
-#clf.fit(X_train, y_train_dt)
-#predictions = clf.predict(X_test)
-#print('Accuracy RandomForest: ' + str(accuracy_score(y_test_dt, predictions)))
-
-
-
 df = pd.DataFrame(data_set)
 unique_dates = df[2].unique()
 
 for i in range(len(unique_dates)):
-	train, test = np.split(data_set, np.where(data_set[:, 2] != unique_dates[i])[0][1:]), np.split(data_set, np.where(data_set[:, 2] == unique_dates[i])[0][1:])
-	train, test = np.vstack(train), np.vstack(test)
+    train, test = np.split(data_set, np.where(data_set[:, 2] != unique_dates[i])[0][1:]), np.split(data_set, np.where(
+        data_set[:, 2] == unique_dates[i])[0][1:])
+    train, test = np.vstack(train), np.vstack(test)
 
-	X_train, Y_train = train[:, 3:-2], train[:,-1]
-	X_test, Y_test = test[:, 3:-2], test[:,-1]
-	Y_train_dt, Y_test_dt = Y_train.astype('int'), Y_test.astype('int')
+    X_train, Y_train = train[:, 3:-2], train[:, -1]
+    X_test, Y_test = test[:, 3:-2], test[:, -1]
+    Y_train_dt, Y_test_dt = Y_train.astype('int'), Y_test.astype('int')
 
+    t = tree.DecisionTreeClassifier()
+    t.fit(X_train, Y_train_dt)
+    pt = t.predict(X_test)
+    print('Accuracy DecisionTree ' + str(i) + ":\t" + str(accuracy_score(Y_test_dt, pt)))
 
-
-	t = tree.DecisionTreeClassifier()
-	t.fit(X_train, Y_train_dt)
-	pt = t.predict(X_test)
-	print('Accuracy DecisionTree ' + str(i) + ":\t" + str(accuracy_score(Y_test_dt, pt)))
-
-	clf = RandomForestClassifier(n_jobs=2, random_state=0)
-	clf.fit(X_train, Y_train_dt)
-	predictions = clf.predict(X_test)
-	print('Accuracy RandomForest ' + str(i) + ':\t' + str(accuracy_score(Y_test_dt, predictions)))
-
-
-
-
-
+    clf = RandomForestClassifier(n_jobs=2, random_state=0)
+    clf.fit(X_train, Y_train_dt)
+    predictions = clf.predict(X_test)
+    print('Accuracy RandomForest ' + str(i) + ':\t' + str(accuracy_score(Y_test_dt, predictions)))
